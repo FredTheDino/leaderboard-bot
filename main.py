c4634b5a3dd7a88e0c87761f5f920f6f101bc075
Edvard Thörnros <edvard.thornros@gmail.com>
2022-06-10 16:07:57 +0200

    Make the graph slightly better

--- main.py
+++ main.py
@@ -20,29 +20,37 @@ def parse_pushup(x):
 
 
 def plot_pushups(pushups_users_dates):
-    user_per_day = defaultdict(lambda: defaultdict(int))
-    for user, data in pushups_users_dates.items():
-        for datetime, number in data.items():
-            user_per_day[user][datetime.date()] += number
-
     total_per_day = defaultdict(int)
     for data in pushups_users_dates.values():
         for datetime, number in data.items():
             total_per_day[datetime.date()] += number
+
+    user_per_day = defaultdict(lambda: defaultdict(int))
+    for user, data in pushups_users_dates.items():
+        for d in total_per_day.keys():
+            user_per_day[user][d] = 0
+        for datetime, number in data.items():
+            user_per_day[user][datetime.date()] += number
 
     total = sum(total_per_day.values())
 
     fix, (ax, bx) = plt.subplots(2, 1)
     for user, data in user_per_day.items():
         label = user.name
-        ax.plot(list(data.keys()), list(data.values()), label=label)
+        xy = sorted(data.items())
+        x = list(map(lambda x:x[0], xy))
+        y = list(map(lambda x:x[1], xy))
+        ax.plot(x, y, label=label, marker='o')
     ax.legend()
     ax.set(xticklabels=[])
     ax.set(title="Pushups per day per person")
     ax.set(xlabel=None)
     ax.set_ylim(ymin=0)
 
-    bx.plot(list(total_per_day.keys()), list(total_per_day.values()), label="total")
+    xy = sorted(total_per_day.items())
+    x = list(map(lambda x:x[0], xy))
+    y = list(map(lambda x:x[1], xy))
+    bx.plot(x, y, label="total", marker='o')
     bx.legend()
     bx.set(xticklabels=[])
     bx.set(title="Total pushups per day")
@@ -50,6 +58,8 @@ def plot_pushups(pushups_users_dates):
     bx.set_ylim(ymin=0)
 
     fix.tight_layout()
+    fix.set_figwidth(10)
+    fix.set_figwidth(10)
     filename = "pushups.png"
     fix.savefig(filename)
     with open(filename, "rb") as f:
@@ -94,7 +104,7 @@ async def on_message(message):
     if command == "recount":
         async with message.channel.typing():
             state = defaultdict(dict)
-            async for message in message.channel.history(limit=200):
+            async for message in message.channel.history(limit=1000):
                 _, state = await note_pushups(state, message)
 
             await send_current_stats(state, message.channel)
